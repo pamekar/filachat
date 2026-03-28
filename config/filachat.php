623d1a0551fa84aef982ec154ac48fb39e3fa479
Victor Ndu <d.pamekar@gmail.com>
2026-03-28 21:29:42 +0100

    Fix MaxWidth to Width enum in config for Filament v4

--- config/filachat.php
+++ config/filachat.php
@@ -243,10 +243,10 @@ return [
     |
     | This option specifies the maximum width of the chat page. You can
     | customize this if you have a different width in your application. You can use
-    | all enum values from \Filament\Support\Enums\MaxWidth.
+    | all enum values from \Filament\Support\Enums\Width.
     |
     */
-    'max_content_width' => \Filament\Support\Enums\MaxWidth::Full,
+    'max_content_width' => \Filament\Support\Enums\Width::Full,
 
     /*
     |--------------------------------------------------------------------------
